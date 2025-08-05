a37fcf2f5a744e41119be2c75c664daec472378c
Berling241 <berlingnilson@gmail.com>
2025-08-05 20:12:18 +0100

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(message)
 function commander(produit, prix) {
-    const numeroWhatsApp = "24166551007"; // Mets ton vrai numéro WhatsApp
+    const numeroWhatsApp = "24170000000"; // Mets ton vrai numéro WhatsApp
     const message = `Bonjour, je souhaite commander : ${produit} (${prix} FCFA) via Darling-Boutique.`;
     const lien = `https://wa.me/${numeroWhatsApp}?text=${encodeURIComponent(message)}`;
     window.open(lien, "_blank");
